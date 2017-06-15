70b6b039a7fc582a15474be6961f3e72faf152aa
USA <huangleisir@qq.com>
2017-06-15 10:14:12 +0800

    通过拿到request拿到session这样可以自己单独拿到csrfToken的值，放在json报文当中来传给app等客户端

--- src/main/java/org/igormukhin/samples/csrfdemoapp/HomeController.java
+++ src/main/java/org/igormukhin/samples/csrfdemoapp/HomeController.java
@@ -2,6 +2,7 @@ package org.igormukhin.samples.csrfdemoapp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.web.csrf.DefaultCsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -29,9 +34,13 @@ public class HomeController {
 
     @RequestMapping(value = "/post", method = POST)
     @ResponseBody
-    public String post() {
-        logger.info("/post called: successful");
-        return "A private POST was successful";
+    public String post(HttpServletRequest request,HttpServletResponse reponse) {
+    	HttpSession session = request.getSession() ;//watch it with debug mode
+    	DefaultCsrfToken defaultCsrfToken = (DefaultCsrfToken)session.getAttribute("org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN");
+        String csrfToken = defaultCsrfToken.getToken();
+        reponse = reponse;
+    	logger.info("/post called: successful  ,csrfToken: "+csrfToken);
+        return "this POST is successful,csrfToken: "+csrfToken;
     }
 
     @RequestMapping(value = "/upload", method = POST)
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -6,7 +6,7 @@
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
-<body onload="document.f.username.focus();">
+<body >   <!--  onload="document.f.username.focus();" -->
 <div class="container">
     <div class="content">
         <c:if test="${param.logout}">
